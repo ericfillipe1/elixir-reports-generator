c8fb1ee6e98764019e86cdb3fd05a5ec12f11b4c
Eric Fillipe <ericfillipework@gmail.com>
2021-05-08 00:29:19 -0400

    Parseando nosso arquivo

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,15 +1,17 @@
 defmodule ReportsGenerator do
-
-  def  build(filename) do
+  def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.map(fn line -> parser_line(line) end)
+    |> Enum.reduce(%{}, fn line, report ->
+      [id, _name, price] = parser_line(line)
+      Map.put(report, id, price)
+    end)
   end
 
   def parser_line(line) do
     line
     |> String.trim()
     |> String.split(",")
-    |> List.update_at(2,&String.to_integer(&1))
+    |> List.update_at(2, &String.to_integer/1)
   end
 end
