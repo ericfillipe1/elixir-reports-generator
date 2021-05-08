555ec55a382e65b413b711f7a2ec06d89c4621a3
Eric Fillipe <ericfillipework@gmail.com>
2021-05-08 01:45:21 -0400

    Calculando a frequência de cada tipo de pedido
    ReportsGenerator.build("report_complete.csv")

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,14 +1,26 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @avariable_foods [
+    "pizza",
+    "açaí",
+    "hambúrguer",
+    "esfirra",
+    "churrasco",
+    "pastel",
+    "prato_feito",
+    "sushi"
+  ]
   def build(filename) do
     "reports/#{filename}"
     |> Parser.from_file_name()
     |> Enum.reduce(report_acc(), &sum_values(&1, &2))
   end
 
-  def sum_values([id, _name, price], report) do
-    Map.put(report, id, report[id] + price)
+  def sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = reports) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+    %{reports | "users" => users, "foods" => foods}
   end
 
   def fetch_hight_cost(report) do
@@ -17,6 +29,9 @@ defmodule ReportsGenerator do
   end
 
   def report_acc() do
-    Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+    foods = Enum.into(@avariable_foods, %{}, &{&1, 0})
+
+    %{"users" => users, "foods" => foods}
   end
 end
