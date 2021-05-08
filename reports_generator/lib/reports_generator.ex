25eb36ad64e56be18e04868f9ed447c35350c039
Eric Fillipe <ericfillipework@gmail.com>
2021-05-08 00:12:23 -0400

    Lendo o arquivo em um stream

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,15 +1,15 @@
 defmodule ReportsGenerator do
-  def build(filename) do
+
+  def  build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handler_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parser_line(line) end)
   end
 
-  def handler_file({:ok, result}) do
-    result
-  end
-
-  def handler_file({:error, _}) do
-    "not found file"
+  def parser_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2,&String.to_integer(&1))
   end
 end
